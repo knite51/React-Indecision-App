6dc05dcadf3772b19e15eba282c95241296f3999
knite51 <knite51@gmail.com>
2018-01-20 19:19:03 +0100

    React Syntax file to render Basic Template

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const template = (
+  <div>
+    <h1>Welcome home Ayotunde Olubiyo.</h1>
+    <p>You are Genius Believe it!</p>
+  </div>
+);
+
+const templateTwo = (
+  <div>
+    <h1>Ayotunde</h1>
+    <p>Age: 23</p>
+    <p>Location: Lagos, Nigeria</p>
+  </div>
+);
+
+ReactDOM.render(templateTwo, document.getElementById('app'));
