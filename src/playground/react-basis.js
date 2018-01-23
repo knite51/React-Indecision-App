b6db98be6b2b7cee2785d673827879417095cf6a
knite51 <knite51@gmail.com>
2018-01-23 08:51:33 +0100

    Updating Basis

--- src/playground/react-basis.js
+++ src/playground/react-basis.js
@@ -1,3 +1,5 @@
+// User Profile
+
 const user = {
   name: 'Ayotunde Olubiyo',
   age: 23,
@@ -51,3 +53,111 @@ const rerender = () => {
 };
 
 rerender();
+
+
+
+//Visibility Toggle
+
+
+let text;
+let buttonInfo = 'Show Details';
+
+const onToggle = () => {
+  if (buttonInfo === 'Show Details') {
+    text = <p>These are some element</p>;
+    buttonInfo = 'Hide Details';
+  } else {
+    text = undefined;
+    buttonInfo = 'Show Details';
+  }
+  rerender();
+}
+
+const rerender = () => {
+  const visible = (
+    <div>
+      <h1>Visibility Toggle</h1>
+      <button onClick={onToggle}>{buttonInfo}</button>
+      {text}
+    </div>
+  );
+  ReactDOM.render(visible, document.getElementById('app'));
+};
+rerender();
+
+//or
+//  let v = false;
+
+//  const t = () => {
+//    v = !v;
+//    render();
+//  };
+
+//  const render = () => {
+//    const j = (
+//      <div>
+//       <h1>Vs</h1>
+//       <button onClick={t}>{v ? 'Hide': 'Show'}</button>
+//       {v && (<p>Yo it worked</p>)}
+//      </div>
+//    )
+//  }
+
+
+
+
+
+// App stage One without Components
+
+const app = {
+  title: 'Indecision App',
+  subTitle: 'Here you go',
+  options: [],
+};
+
+const onFormSubmit = (e) => {
+  e.preventDefault();
+  const value = e.target.elements.option.value;
+  if (value) {
+    app.options.push(e.target.elements.option.value);
+    e.target.elements.option.value = '';
+    rerenderForm();
+  }
+}
+
+const removeOptions = () => {
+  app.options = [];
+  rerenderForm();
+};
+
+const onClickDecision = () => {
+  let randomNum = Math.floor(Math.random() * app.options.length);
+  let option = app.options[randomNum];
+  alert(option);
+}
+
+const rerenderForm = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subTitle && <h1>{app.subTitle}</h1>}
+      <p>{app.options.length > 0 ? 'Here are your options' : 'No Options'}</p>
+      <button disabled={app.options.length <= 0} onClick={onClickDecision}>What Should I do</button>
+      <button onClick={removeOptions}>RemoveAll</button>
+      <ol>
+        {
+          app.options.map((option) => {
+            return <li key={option}>{option}</li>
+          })
+        }
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type='text' name='option' />
+        <button>Add Option</button>
+      </form>
+    </div>
+  );
+
+  ReactDOM.render(template, document.getElementById('app'));
+};
+rerenderForm();
